d9e6ac72700e96c5863944f00546ecf2e78b0bf1
Ashley Tolen <tolenashley7@gmail.com>
2019-06-21 05:03:50 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,28 @@
+require 'pry'
 # Write your code here.
+def dictionary(words, substitutes)
+  {
+  "hello": "hi",
+  "to, two, too": 2,
+  "for, four": 4,
+  "be": "b",
+  "you": "u",
+  "at": "@",
+  "and": "&"
+  }
+end
+
+def word_substituter(tweet)
+  array_of_tweets = tweet.split()
+  new_tweet = []
+  
+  dictionary.each do |words, substitutes|
+    array_of_tweets.each do |words|
+      if word == dictionary.key
+        new_tweet << dictionary.value
+      end
+    end
+  end
+  new_tweet
+  binding.pry
+end
